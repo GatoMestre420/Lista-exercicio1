d553d737ec9a88c7da0912fa7b1bc2af949082ed
GatoMestre420 <luancurt@outlook.com>
2024-03-25 10:00:06 -0300

    ex8

---
+++ Ex08.java
@@ -0,0 +1,20 @@
+
+
+public class Ex08 {
+    public static void executar(){
+      
+
+        System.out.println("Informe um numero: ");
+        int num = Prompt.lerInteiro();
+
+        if(num >= 50){
+            System.out.println("Esse numero é maior que 50");
+        }else{
+            System.out.println("Esse numero é menor que 50");
+        }
+
+
+       
+    }
+
+}
