ab87c7ad5193d743fe5bbd33539d0c804b539ac7
GatoMestre420 <luancurt@outlook.com>
2024-03-25 09:55:46 -0300

    ex1

---
+++ Ex01.java
@@ -0,0 +1,21 @@
+
+public class Ex01 {
+    public static void executar(){
+
+        int numero;
+        
+        System.out.println("Informe um numero");
+         numero = Prompt.lerInteiro();
+
+        if(numero > 10){
+            System.out.println("Numero maior que 10!!");
+        }else if(numero <= 10){
+            System.out.println("Numero menor ou igual a 10!!");
+
+        }
+        
+    
+
+    }
+
+}
