a88c1e068670b03cb9ba70c1652587cec33b0f1c
GatoMestre420 <luancurt@outlook.com>
2024-03-25 09:58:43 -0300

    ex4

---
+++ Ex04.java
@@ -0,0 +1,23 @@
+
+
+public class Ex04 {
+    public static void executar(){
+    
+        
+
+        System.out.println("Informe o primeiro numero: ");
+        int num1 = Prompt.lerInteiro();
+        System.out.println("Informe o segundo numero: ");
+        int num2 = Prompt.lerInteiro();
+
+        System.out.println("A soma dos dois numeros é: " + (num1 + num2));
+        System.out.println("A subtração dos dois numeros é: " + (num1 - num2));
+        System.out.println("A multiplicação dos dois numeros é: " + (num1 * num2));
+        System.out.println("A divisão dos dois números é: " + (num1 / num2));
+
+
+       
+
+    }
+
+}
