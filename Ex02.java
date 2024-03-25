c4e233c15d46899d02f4a7df72e54f4779698ed1
GatoMestre420 <luancurt@outlook.com>
2024-03-25 09:57:54 -0300

    ex2

---
+++ Ex02.java
@@ -0,0 +1,23 @@
+
+
+public class Ex02 {
+    public static void executar(){
+
+        
+
+        System.out.println("Informe o Primeiro Número: ");
+        int num1 = Prompt.lerInteiro();
+        System.out.println("Informe o Segundo Número: ");
+        int num2 = Prompt.lerInteiro();
+
+        System.out.println("A soma dos dois numeros foi: " + (num1 + num2));
+
+       
+
+    }
+
+    
+
+
+
+}
